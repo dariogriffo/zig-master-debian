8c26d217a12c7ea7fea5042a8e1a707d2eefd2c1
Dario Griffo <dariogriffo@gmail.com>
2025-07-13 18:08:46 +0100

    Fix build

--- build_zig_debian.sh
+++ build_zig_debian.sh
@@ -5,10 +5,10 @@ for i in "${arr[@]}"
 do
   DEBIAN_DIST=$i
   FULL_VERSION=$ZIG_VERSION-${BUILD_VERSION}+${DEBIAN_DIST}_amd64
-  docker build . -t zig-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION
-  id="$(docker create zig-$DEBIAN_DIST)"
-  docker cp $id:/zig_$FULL_VERSION.deb - > ./zig_$FULL_VERSION.deb
-  tar -xf ./zig_$FULL_VERSION.deb
+  docker build . -t zig-master-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION
+  id="$(docker create zig-master-$DEBIAN_DIST)"
+  docker cp $id:/zig-master_$FULL_VERSION.deb - > ./zig-master_$FULL_VERSION.deb
+  tar -xf ./zig-master_$FULL_VERSION.deb
 done
 
   
